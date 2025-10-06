68e440c172cdd9ce1f758a0ebc928dfa9f54e627
fjktkm <fjktkm@gmail.com>
2025-10-06 19:08:32 +0000

    Refactor main.py

--- main.py
+++ main.py
@@ -1,301 +1,263 @@
 import struct
+from itertools import accumulate
 from pathlib import Path
 
 
-def u16(x):
-    return struct.pack(">H", x & 0xFFFF)
+def u16(x: int) -> bytes:
+    return x.to_bytes(2, "big", signed=False)
 
 
-def i16(x):
-    return struct.pack(">h", ((x + 0x8000) & 0xFFFF) - 0x8000)
+def i16(x: int) -> bytes:
+    return x.to_bytes(2, "big", signed=True)
 
 
-def u32(x):
-    return struct.pack(">I", x & 0xFFFFFFFF)
+def u32(x: int) -> bytes:
+    return x.to_bytes(4, "big", signed=False)
 
 
-def u64(x):
-    return struct.pack(">Q", x & 0xFFFFFFFFFFFFFFFF)
+def u64(x: int) -> bytes:
+    return x.to_bytes(8, "big", signed=False)
 
 
 UNITS_PER_EM = 1000
 ASCENT = 800
 DESCENT = -200
 LINE_GAP = 0
-ADV_WIDTH_0 = 500
-ADV_WIDTH_1 = 500
+ADV_WIDTH = 500
 XMIN, YMIN, XMAX, YMAX = 0, 0, 500, 700
 
 
 def build_glyf_and_loca():
-    def build_simple_glyph(contours, bbox):
-        xMin, yMin, xMax, yMax = bbox
-        endPts = []
-        flags = []
-        xs = []
-        ys = []
-        idx = 0
-        for pts in contours:
-            endPts.append(idx + len(pts) - 1)
-            for x, y in pts:
-                flags.append(0x01)
-                xs.append(x)
-                ys.append(y)
-            idx += len(pts)
-        x_d = [xs[0]] + [xs[i] - xs[i - 1] for i in range(1, len(xs))] if xs else []
-        y_d = [ys[0]] + [ys[i] - ys[i - 1] for i in range(1, len(ys))] if ys else []
-        return (
-            i16(len(contours))
-            + i16(xMin)
-            + i16(yMin)
-            + i16(xMax)
-            + i16(yMax)
-            + b"".join(u16(v) for v in endPts)
-            + u16(0)
-            + bytes(flags)
-            + b"".join(i16(v) for v in x_d)
-            + b"".join(i16(v) for v in y_d)
-        )
+    pts = [(XMIN, YMIN), (XMAX, YMIN), (XMAX, YMAX), (XMIN, YMAX)]
+    xs = [x for x, _ in pts]
+    ys = [y for _, y in pts]
+    x_d = [xs[0], xs[1] - xs[0], xs[2] - xs[1], xs[3] - xs[2]]
+    y_d = [ys[0], ys[1] - ys[0], ys[2] - ys[1], ys[3] - ys[2]]
 
-    notdef_pts = [(0, 0), (500, 0), (500, 700), (0, 700)]
-    notdef = build_simple_glyph([notdef_pts], (XMIN, YMIN, XMAX, YMAX))
-    space = i16(0) + i16(0) + i16(0) + i16(0) + i16(0) + u16(0)
-    glyf = notdef + space
-    loca = u32(0) + u32(len(notdef)) + u32(len(glyf))
+    notdef = (
+        i16(1)  # numberOfContours
+        + i16(XMIN)  # xMin
+        + i16(YMIN)  # yMin
+        + i16(XMAX)  # xMax
+        + i16(YMAX)  # yMax
+        + u16(3)  # endPtsOfContours
+        + u16(0)  # instructionLength
+        + bytes([0x01, 0x01, 0x01, 0x01])  # flags
+        + b"".join(i16(v) for v in x_d)  # xCoordinates
+        + b"".join(i16(v) for v in y_d)  # yCoordinates
+    )
+
+    glyf = notdef
+    loca = u32(0) + u32(len(glyf))
     return glyf, loca
 
 
 def build_hmtx():
-    return u16(ADV_WIDTH_0) + i16(0) + u16(ADV_WIDTH_1) + i16(0)
+    return (
+        u16(ADV_WIDTH)  # advanceWidth
+        + i16(0)  # lsb (leftSideBearing)
+    )
 
 
 def build_hhea():
-    advanceWidthMax = max(ADV_WIDTH_0, ADV_WIDTH_1)
     xMaxExtent = XMAX - XMIN
     return (
-        u32(0x00010000)
-        + i16(ASCENT)
-        + i16(DESCENT)
-        + i16(LINE_GAP)
-        + u16(advanceWidthMax)
-        + i16(0)
-        + i16(0)
-        + i16(xMaxExtent)
-        + i16(1)
-        + i16(0)
-        + i16(0)
-        + i16(0)
-        + i16(0)
-        + i16(0)
-        + i16(0)
-        + u16(0)
-        + u16(2)
+        u32(0x00010000)  # tableVersion
+        + i16(ASCENT)  # ascender
+        + i16(DESCENT)  # descender
+        + i16(LINE_GAP)  # lineGap
+        + u16(ADV_WIDTH)  # advanceWidthMax
+        + i16(0)  # minLeftSideBearing
+        + i16(0)  # minRightSideBearing
+        + i16(xMaxExtent)  # xMaxExtent
+        + i16(1)  # caretSlopeRise
+        + i16(0)  # caretSlopeRun
+        + i16(0)  # caretOffset
+        + i16(0)  # reserved1
+        + i16(0)  # reserved2
+        + i16(0)  # reserved3
+        + i16(0)  # reserved4
+        + u16(0)  # metricDataFormat
+        + u16(1)  # numberOfHMetrics
     )
 
 
 def build_maxp():
     return (
-        u32(0x00010000)
-        + u16(2)
-        + u16(4)
-        + u16(1)
-        + u16(0)
-        + u16(0)
-        + u16(2)
-        + u16(0)
-        + u16(0)
-        + u16(0)
-        + u16(0)
-        + u16(0)
-        + u16(0)
-        + u16(0)
-        + u16(0)
+        u32(0x00010000)  # tableVersion
+        + u16(1)  # numGlyphs
+        + u16(4)  # maxPoints
+        + u16(1)  # maxContours
+        + u16(0)  # maxCompositePoints
+        + u16(0)  # maxCompositeContours
+        + u16(0)  # maxZones
+        + u16(0)  # maxTwilightPoints
+        + u16(0)  # maxStorage
+        + u16(0)  # maxFunctionDefs
+        + u16(0)  # maxInstructionDefs
+        + u16(0)  # maxStackElements
+        + u16(0)  # maxSizeOfInstructions
+        + u16(0)  # maxComponentElements
+        + u16(0)  # maxComponentDepth
     )
 
 
 def build_post():
-    version = 0x00030000
-    italicAngle = 0
-    underlinePosition = -75
-    underlineThickness = 50
-    isFixedPitch = 0
-    minMemType42 = 0
-    maxMemType42 = 0
-    minMemType1 = 0
-    maxMemType1 = 0
     return (
-        u32(version)
-        + u32(italicAngle)
-        + i16(underlinePosition)
-        + i16(underlineThickness)
-        + u32(isFixedPitch)
-        + u32(minMemType42)
-        + u32(maxMemType42)
-        + u32(minMemType1)
-        + u32(maxMemType1)
+        u32(0x00030000)  # version
+        + u32(0)  # italicAngle
+        + i16(0)  # underlinePosition
+        + i16(0)  # underlineThickness
+        + u32(0)  # isFixedPitch
+        + u32(0)  # minMemType42
+        + u32(0)  # maxMemType42
+        + u32(0)  # minMemType1
+        + u32(0)  # maxMemType1
     )
 
 
 def build_name():
-    def enc16(s):
-        return s.encode("utf-16-be")
-
-    PLATFORM_MS = 3
-    ENC_MS_UNICODE_BMP = 1
-    LANG_EN_US = 0x0409
-    NAME_FAMILY = 1
-    NAME_SUBFAMILY = 2
-    NAME_UNIQUE = 3
-    NAME_FULL = 4
-    NAME_VERSION = 5
-    NAME_POSTSCRIPT = 6
-    family = "FontFromScratch"
-    style = "Regular"
-    full = "FontFromScratch Regular"
-    ps = "FontFromScratch-Regular"
-    version = "Version 1.000"
-    unique = family + " 1.0"
-    recs = [
-        (PLATFORM_MS, ENC_MS_UNICODE_BMP, LANG_EN_US, NAME_FAMILY, enc16(family)),
-        (PLATFORM_MS, ENC_MS_UNICODE_BMP, LANG_EN_US, NAME_SUBFAMILY, enc16(style)),
-        (PLATFORM_MS, ENC_MS_UNICODE_BMP, LANG_EN_US, NAME_UNIQUE, enc16(unique)),
-        (PLATFORM_MS, ENC_MS_UNICODE_BMP, LANG_EN_US, NAME_FULL, enc16(full)),
-        (PLATFORM_MS, ENC_MS_UNICODE_BMP, LANG_EN_US, NAME_VERSION, enc16(version)),
-        (PLATFORM_MS, ENC_MS_UNICODE_BMP, LANG_EN_US, NAME_POSTSCRIPT, enc16(ps)),
+    items = [
+        (1, "FontFromScratch"),
+        (2, "Regular"),
+        (3, "FontFromScratch 1.0"),
+        (4, "FontFromScratch Regular"),
+        (5, "Version 1.000"),
+        (6, "FontFromScratch-Regular"),
     ]
-    count = len(recs)
-    stringOffset = 6 + count * 12
-    header = u16(0) + u16(count) + u16(stringOffset)
-    entries = []
-    strings = []
-    off = 0
-    for pl, en, lang, nid, bs in recs:
-        entries.append(struct.pack(">HHHHHH", pl, en, lang, nid, len(bs), off))
-        strings.append(bs)
-        off += len(bs)
+
+    strings = [s.encode("utf-16-be") for _, s in items]
+    lengths = [len(bs) for bs in strings]
+    offsets = [sum(lengths[:i]) for i in range(len(lengths))]
+
+    entries = [
+        struct.pack(">HHHHHH", 3, 1, 0x0409, nid, ln, off)
+        for (nid, _), ln, off in zip(items, lengths, offsets)
+    ]
+
+    count = len(entries)
+    header = u16(0) + u16(count) + u16(6 + count * 12)
+
     return header + b"".join(entries) + b"".join(strings)
 
 
 def build_cmap():
-    PLATFORM_MS = 3
-    ENC_MS_UNICODE_BMP = 1
+    sub13 = (
+        u16(13)  # format
+        + u16(0)  # reserved
+        + u32(28)  # length
+        + u32(0)  # language
+        + u32(1)  # numGroups
+        + u32(0x00000000)  # startCharCode
+        + u32(0x0010FFFF)  # endCharCode
+        + u32(0)  # startGlyphID
+    )
 
-    def format4(mapping):
-        def log2floor(n):
-            p = 0
-            while (1 << (p + 1)) <= n:
-                p += 1
-            return p
+    sub4 = (
+        u16(4)  # format
+        + u16(32)  # length
+        + u16(0)  # language
+        + u16(4)  # segCountX2
+        + u16(4)  # searchRange
+        + u16(1)  # entrySelector
+        + u16(0)  # rangeShift
+        + u16(0x0041)  # endCode[0]
+        + u16(0xFFFF)  # endCode[1]
+        + u16(0)  # reservedPad
+        + u16(0x0041)  # startCode[0]
+        + u16(0xFFFF)  # startCode[1]
+        + u16((-0x0041) & 0xFFFF)  # idDelta[0]
+        + u16(1)  # idDelta[1]
+        + u16(0)  # idRangeOffset[0]
+        + u16(0)  # idRangeOffset[1]
+    )
 
-        endCodes = [0x0000, 0x0020, 0xFFFF]
-        startCodes = [0x0000, 0x0020, 0xFFFF]
-        idDeltas = [
-            (mapping.get(0x0000, 0) - 0x0000) & 0xFFFF,
-            (mapping.get(0x0020, 0) - 0x0020) & 0xFFFF,
-            1,
-        ]
-        idRangeOffsets = [0, 0, 0]
-        segCount = 3
-        segCountX2 = segCount * 2
-        entrySelector = log2floor(segCount)
-        searchRange = 2 * (1 << entrySelector)
-        rangeShift = segCountX2 - searchRange
-        length = 14 + segCountX2 * 4 + 2
-        return (
-            u16(4)
-            + u16(length)
-            + u16(0)
-            + u16(segCountX2)
-            + u16(searchRange)
-            + u16(entrySelector)
-            + u16(rangeShift)
-            + b"".join(u16(x) for x in endCodes)
-            + u16(0)
-            + b"".join(u16(x) for x in startCodes)
-            + b"".join(u16(x) for x in idDeltas)
-            + b"".join(u16(x) for x in idRangeOffsets)
-        )
-
-    sub = format4({0x0000: 0, 0x0020: 1})
-    header = u16(0) + u16(1) + struct.pack(">HHI", PLATFORM_MS, ENC_MS_UNICODE_BMP, 12)
-    return header + sub
+    num = 2
+    off0 = 4 + num * 8
+    off1 = off0 + len(sub13)
+    header = (
+        u16(0)  # version
+        + u16(num)  # numberSubtables
+    )
+    recs = struct.pack(">HHI", 3, 10, off0) + struct.pack(">HHI", 3, 1, off1)
+    return header + recs + sub13 + sub4
 
 
 def build_head():
     return (
-        u32(0x00010000)
-        + u32(0x00010000)
-        + u32(0)
-        + u32(0x5F0F3CF5)
-        + u16(0)
-        + u16(UNITS_PER_EM)
-        + u64(0)
-        + u64(0)
-        + i16(XMIN)
-        + i16(YMIN)
-        + i16(XMAX)
-        + i16(YMAX)
-        + u16(0)
-        + u16(8)
-        + i16(2)
-        + i16(1)
-        + i16(0)
+        u32(0x00010000)  # majorVersion | minorVersion
+        + u32(0x00010000)  # fontRevision
+        + u32(0)  # checkSumAdjustment
+        + u32(0x5F0F3CF5)  # magicNumber
+        + u16(0)  # flags
+        + u16(UNITS_PER_EM)  # unitsPerEm
+        + u64(0)  # created
+        + u64(0)  # modified
+        + i16(XMIN)  # xMin
+        + i16(YMIN)  # yMin
+        + i16(XMAX)  # xMax
+        + i16(YMAX)  # yMax
+        + u16(0)  # macStyle
+        + u16(0)  # lowestRecPPEM
+        + i16(0)  # fontDirectionHint
+        + i16(1)  # indexToLocFormat
+        + i16(0)  # glyphDataFormat
     )
 
 
-def build_os2_v2():
+def build_os2():
     return (
-        u16(2)
-        + i16(500)
-        + u16(400)
-        + u16(5)
-        + u16(0)
-        + u16(650)
-        + u16(699)
-        + i16(0)
-        + i16(140)
-        + u16(650)
-        + u16(699)
-        + i16(0)
-        + i16(479)
-        + u16(50)
-        + i16(300)
-        + i16(0)
-        + bytes(10)
-        + u32(1)
-        + u32(0)
-        + u32(0)
-        + u32(0)
-        + b"NONE"
-        + u16(0x0040)
-        + u16(0x0000)
-        + u16(0x0020)
-        + i16(ASCENT)
-        + i16(DESCENT)
-        + i16(0)
-        + u16(ASCENT)
-        + u16(-DESCENT)
-        + u32(1)
-        + u32(0)
-        + i16(500)
-        + i16(700)
-        + u16(0)
-        + u16(32)
-        + u16(1)
+        u16(2)  # version
+        + i16(0)  # xAvgCharWidth
+        + u16(0)  # usWeightClass
+        + u16(0)  # usWidthClass
+        + u16(0)  # fsType
+        + u16(0)  # ySubscriptXSize
+        + u16(0)  # ySubscriptYSize
+        + i16(0)  # ySubscriptXOffset
+        + i16(0)  # ySubscriptYOffset
+        + u16(0)  # ySuperscriptXSize
+        + u16(0)  # ySuperscriptYSize
+        + i16(0)  # ySuperscriptXOffset
+        + i16(0)  # ySuperscriptYOffset
+        + u16(0)  # yStrikeoutSize
+        + i16(0)  # yStrikeoutPosition
+        + i16(0)  # sFamilyClass
+        + bytes(10)  # panose
+        + u32(0)  # ulUnicodeRange1
+        + u32(0)  # ulUnicodeRange2
+        + u32(0)  # ulUnicodeRange3
+        + u32(0)  # ulUnicodeRange4
+        + b"NONE"  # achVendID
+        + u16(0)  # fsSelection
+        + u16(0)  # usFirstCharIndex
+        + u16(0xFFFF)  # usLastCharIndex
+        + i16(ASCENT)  # sTypoAscender
+        + i16(DESCENT)  # sTypoDescender
+        + i16(0)  # sTypoLineGap
+        + u16(ASCENT)  # usWinAscent
+        + u16(-DESCENT)  # usWinDescent
+        + u32(0)  # ulCodePageRange1
+        + u32(0)  # ulCodePageRange2
+        + i16(0)  # sxHeight
+        + i16(0)  # sCapHeight
+        + u16(0)  # usDefaultChar
+        + u16(0)  # usBreakChar
+        + u16(0)  # usMaxContext
     )
 
 
 def build_tables():
     glyf_raw, loca_raw = build_glyf_and_loca()
     return {
-        b"OS/2": build_os2_v2(),
-        b"cmap": build_cmap(),
-        b"glyf": glyf_raw,
         b"head": build_head(),
         b"hhea": build_hhea(),
-        b"hmtx": build_hmtx(),
-        b"loca": loca_raw,
         b"maxp": build_maxp(),
+        b"OS/2": build_os2(),
+        b"hmtx": build_hmtx(),
+        b"cmap": build_cmap(),
+        b"loca": loca_raw,
+        b"glyf": glyf_raw,
         b"name": build_name(),
         b"post": build_post(),
     }
@@ -307,16 +269,13 @@ def build_sfnt(tables):
 
     def sfnt_checksum(data: bytes) -> int:
         d = pad4(data)
-        s = 0
-        for i in range(0, len(d), 4):
-            s = (s + struct.unpack(">I", d[i : i + 4])[0]) & 0xFFFFFFFF
-        return s
+        return (
+            sum(struct.unpack(">I", d[i : i + 4])[0] for i in range(0, len(d), 4))
+            & 0xFFFFFFFF
+        )
 
     def log2floor(n: int) -> int:
-        p = 0
-        while (1 << (p + 1)) <= n:
-            p += 1
-        return p
+        return (n.bit_length() - 1) if n > 0 else 0
 
     tags = sorted(tables.keys())
     numTables = len(tags)
@@ -325,20 +284,22 @@ def build_sfnt(tables):
     entrySelector = log2floor(numTables)
     rangeShift = numTables * 16 - searchRange
 
-    offset = 12 + numTables * 16
-    entries = []
-    payload = []
-    offsets = {}
-    for t in tags:
-        raw = tables[t]
-        length = len(raw)
-        cs = sfnt_checksum(raw if t != b"head" else raw[:8] + b"\0\0\0\0" + raw[12:])
-        entries.append(t + u32(cs) + u32(offset) + u32(length))
-        offsets[t] = offset
-        part = pad4(raw)
-        payload.append(part)
-        offset += len(part)
+    raws = [tables[t] for t in tags]
+    lengths = [len(r) for r in raws]
+    checksums = [
+        sfnt_checksum(r if t != b"head" else r[:8] + b"\0\0\0\0" + r[12:])
+        for t, r in zip(tags, raws)
+    ]
+    parts = [pad4(r) for r in raws]
 
+    base = 12 + numTables * 16
+    offsets_list = [base + o for o in accumulate([0] + [len(p) for p in parts])][:-1]
+    offsets = {t: o for t, o in zip(tags, offsets_list)}
+
+    entries = [
+        t + u32(cs) + u32(off) + u32(length)
+        for t, cs, off, length in zip(tags, checksums, offsets_list, lengths)
+    ]
     header = (
         u32(0x00010000)
         + u16(numTables)
@@ -347,7 +308,9 @@ def build_sfnt(tables):
         + u16(rangeShift)
     )
     directory = b"".join(entries)
-    font = bytearray(header + directory + b"".join(payload))
+    payload = b"".join(parts)
+
+    font = bytearray(header + directory + payload)
 
     head_off = offsets[b"head"]
     font[head_off + 8 : head_off + 12] = b"\0\0\0\0"
